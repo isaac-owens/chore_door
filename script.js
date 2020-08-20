a1870d66fb5f1e3c5c55b8777a7a1db78878551a
Isaac Owens <isaac.owens832@gmail.com>
2020-08-20 13:14:00 -0400

    Add random chore door generator function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let doorImage1 = document.getElementById("door1");
-let doorImage2 = document.getElementById("door2");
-let doorImage3 = document.getElementById("door3");
+const doorImage1 = document.getElementById("door1");
+const doorImage2 = document.getElementById("door2");
+const doorImage3 = document.getElementById("door3");
 
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
-let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
+const numClosedDoors = 3;
+
+randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoors);
+}
 
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
