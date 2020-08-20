0078c926e4652ca0a08f08c80b9d314b2317d07e
Isaac Owens <isaac.owens832@gmail.com>
2020-08-20 13:21:49 -0400

    Add swith statements for random values in random function

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
+const openDoor1;
+const openDoor2;
+const openDoor3;
+
 const numClosedDoors = 3;
 
 randomChoreDoorGenerator = () => {
   const choreDoor = Math.floor(Math.random() * numClosedDoors);
+  switch (choreDoor) {
+    case 0:
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case 1: 
+      openDoor2 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    default: 
+      openDoor3 = botDoorPath;
+      openDoor1 = spaceDoorPath;
+      openDoor2 = beachDoorPath;
+  }
 }
 
 doorImage1.onclick = () => {
