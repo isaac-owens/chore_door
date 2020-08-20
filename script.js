585cdc7664d1916ee37a8e74e0602e217b572dab
Isaac Owens <isaac.owens832@gmail.com>
2020-08-20 12:50:31 -0400

    Add on clicks and globar variables for doors 2 and 3

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+
 let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
+}
+
+doorImage2.onclick = () => {
+  doorImage2.src = botDoorPath;
+}
+
+doorImage3.onclick = () => {
+  doorImage3.src = botDoorPath;
 }
