800a21d06757be9da056e378710c39e884454875
Isaac Owens <isaac.owens832@gmail.com>
2020-08-20 13:24:13 -0400

    Implement and test randomDoorGenerator

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
-const openDoor1;
-const openDoor2;
-const openDoor3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
 
 const numClosedDoors = 3;
 
@@ -33,13 +33,15 @@ randomChoreDoorGenerator = () => {
 }
 
 doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 }
 
 doorImage2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 }
 
 doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 }
+
+randomChoreDoorGenerator();
